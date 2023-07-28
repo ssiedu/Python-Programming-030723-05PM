798e2dc7b99cbecd2e590c30aa4e3e21ac573694
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-07-28 19:28:05 +0530

    program

---
+++ alphabetseries.py
@@ -0,0 +1,5 @@
+i=ord(input("Enter start alphabets :"))
+n=ord(input("Enter end alphabets series:"))
+while i<=n:
+    print(chr(i),end=" ")
+    i=i+1
---
+++ while1.py
@@ -0,0 +1,10 @@
+
+i=int(input("Enter start value :"))
+n=int(input("Enter end value :"))
+
+while(i<=n):
+    print(i,end=" ")
+    #print("Hello World")
+    i=i+1
+#print()
+print("\nThank you")
